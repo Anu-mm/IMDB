9dffac6a6bd5be9e72c21c984a0309ccff1519a5
Anu-mm <138592948+Anu-mm@users.noreply.github.com>
2023-08-15 22:11:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const movieList = document.getElementById("movie-list");
+    const addMovieForm = document.getElementById("add-movie-form");
+
+    addMovieForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const titleInput = document.getElementById("title");
+        const genreInput = document.getElementById("genre");
+
+        const title = titleInput.value;
+        const genre = genreInput.value;
+
+        if (title.trim() !== "" && genre.trim() !== "") {
+            const movieItem = document.createElement("li");
+            movieItem.classList.add("list-group-item");
+            movieItem.innerHTML = `<strong>${title}</strong> - ${genre}`;
+            movieList.appendChild(movieItem);
+
+            titleInput.value = "";
+            genreInput.value = "";
+        }
+    });
+});
 
